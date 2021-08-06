7f500cefe0efdc926e033890b64eeefb046aee49
Andrii Konovalov <78738364+andysmokk@users.noreply.github.com>
2021-08-06 17:17:31 +0200

    add color switch

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import colors from "./colors.js";
+
+const refs = {
+  startBtnRef: document.querySelector('[data-action="start"]'),
+  stopBtnRef: document.querySelector('[data-action="stop"]'),
+  body: document.querySelector("body"),
+};
+
+let timerId = null;
+
+refs.startBtnRef.addEventListener("click", onStartBtnClick);
+refs.stopBtnRef.addEventListener("click", onStopBtnClick);
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+function onStartBtnClick() {
+  runColorSwich();
+  refs.startBtnRef.setAttribute("disabled", true);
+}
+
+function onStopBtnClick() {
+  stopColorSwich();
+  refs.startBtnRef.removeAttribute("disabled");
+}
+
+function runColorSwich() {
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor =
+      colors[randomIntegerFromInterval(0, colors.length - 1)];
+  }, 1000);
+}
+
+function stopColorSwich() {
+  clearInterval(timerId);
+  refs.body.style.backgroundColor = null;
+}
